f75dd0175f5b40cf172097778d8f665da319d863
Xeniya Rogers <sana@xeniyas-mbp.lan>
2021-02-28 12:40:51 -0500

    updates

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 from splinter import Browser
 import time
@@ -40,13 +34,9 @@ def mars_news(browser):
     
 
 # ### JPL Mars Space Images - Featured Image
-# 
 # * Visit the url for JPL Featured Space Image [here](https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html).
-# 
 # * Use splinter to navigate the site and find the image url for the current Featured Mars Image and assign the url string to a variable called `featured_image_url`.
-# 
 # * Make sure to find the image url to the full size `.jpg` image.
-# 
 # * Make sure to save a complete url string for this image.
 
 # In[4]:
@@ -59,9 +49,7 @@ def mars_images(browser):
     return image
 
 # ### Mars Facts
-# 
 # * Visit the Mars Facts webpage [here](https://space-facts.com/mars/) and use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
-# 
 # * Use Pandas to convert the data to a HTML table string.
 
 # In[5]:
@@ -71,13 +59,9 @@ def mars_facts():
 
 
 # ### Mars Hemispheres
-# 
 # * Visit the USGS Astrogeology site [here](https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars) to obtain high resolution images for each of Mar's hemispheres.
-# 
 # * You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
-# 
 # * Save both the image url string for the full resolution hemisphere image, and the Hemisphere title containing the hemisphere name. Use a Python dictionary to store the data using the keys `img_url` and `title`.
-# 
 # * Append the dictionary with the image url string and the hemisphere title to a list. This list will contain one dictionary for each hemisphere.
 
 # In[6]:
